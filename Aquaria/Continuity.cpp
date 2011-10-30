a5b756afe0c0988b4d7f7f78a98f336ad58da5f4
fgenesis <false.genesis@googlemail.com>
2011-10-31 00:44:17 +0100

    respect user settings before enabling post FX

--- Aquaria/Continuity.cpp
+++ Aquaria/Continuity.cpp
@@ -1984,12 +1984,15 @@ void Continuity::applyWorldEffects(WorldType type, bool transition, bool affectM
 	if (type == WT_SPIRIT)
 	{
 		
-		//core->postProcessingFx.format = GL_LUMINANCE;
-		core->postProcessingFx.blendType = 1;
-		core->postProcessingFx.intensity = 0.2f;
-		core->postProcessingFx.layer = LR_AFTER_EFFECTS;//LR_AFTER_EFFECTS;
-		core->postProcessingFx.renderLayer = LR_AFTER_EFFECTS;
-		core->postProcessingFx.enable(FXT_RADIALBLUR);
+        if(dsq->user.video.blur)
+        {
+		    //core->postProcessingFx.format = GL_LUMINANCE;
+		    core->postProcessingFx.blendType = 1;
+		    core->postProcessingFx.intensity = 0.2f;
+		    core->postProcessingFx.layer = LR_AFTER_EFFECTS;//LR_AFTER_EFFECTS;
+		    core->postProcessingFx.renderLayer = LR_AFTER_EFFECTS;
+		    core->postProcessingFx.enable(FXT_RADIALBLUR);
+        }
 		
 		dsq->game->avatar->canWarp = false;
 
